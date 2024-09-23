353ea616f00fd14dc9b37c55f1908b296f2e9d4b
leconstantin <lcon69184@gmail.com>
2024-09-23 19:49:31 +0200

    Add files via upload

---
+++ images/index.js
@@ -0,0 +1,21 @@
+
+import logo from './logo.png'
+import headerLogo from './header-logo.svg'
+import principal from './princ.jpg'
+import user1 from './avatar4.jpg'
+import user2 from './avatar3.jpg'
+import user3 from './tm1.jpg'
+import user4 from './tm4.jpg'
+import user5 from './tm5.jpg'
+import user6 from './1.jpg'
+export {
+    user1,
+    user2,
+    user3,
+    user4,
+    user5,
+    user6,
+    logo,
+    headerLogo,
+    principal
+}
